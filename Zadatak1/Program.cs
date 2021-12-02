8085d7a8b7fc63359b70d99142c9e8f8cfe9516b
Luka Gregorić <endo145@gmail.com>
2021-12-02 18:32:35 +0100

    Resolve 03b task

--- Zadatak1/Program.cs
+++ Zadatak1/Program.cs
@@ -9,18 +9,38 @@ namespace Vsite.CSharp.Labos4.Zadatak1
     // TODO:03a Definirati klasu koja predstavlja studenta i klasu koja predstavlja ispit.
     // Klasa Ispit sadrži članove Naziv i Ocjena, a klasa Student sadrži članove ImePrezime i listu Ispit[] ispita na koje je student izašao.
 
+    class Ispit
+    {
+        public string Naziv { get; set; }
+        public int Ocjena { get; set; }
+    }
+    class Student
+    {
+        public string ImePrezime { get; set; }
+        public Ispit[] Ispiti { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // TODO:03b Zadati listu studenata s popisom ispita na koje je izašao.
-
+            // 03b Zadati listu studenata s popisom ispita na koje je izašao.
+            var studenti = new List<Student>
+            {
+                new Student{ImePrezime = "Pero Perić", Ispiti = new Ispit[] { new Ispit{Naziv = "Matematika", Ocjena = 1} } },
+                new Student{ImePrezime = "Ivo Ivic", Ispiti = new Ispit[] { new Ispit{Naziv = "Fizika", Ocjena = 5}, new Ispit { Naziv = "Povijest", Ocjena = 2 } } },
+                new Student{ImePrezime = "Marko Markic", Ispiti = new Ispit[] { new Ispit{Naziv = "Zemljopis", Ocjena = 3}, new Ispit{Naziv = "Matematika", Ocjena = 4}, new Ispit { Naziv = "Povijest", Ocjena = 5 } } },
+                new Student{ImePrezime = "Lovro Lovric", Ispiti = new Ispit[] { } }
+            };
             string imePredmeta = "Matematika";
             Console.WriteLine("Studenti koji su izašli na ispit iz predmeta {imePredmeta}");
             // TODO:03c Napisati upit koji će vratiti popis svih studenata koji su izašli na ispit iz zadanog predmeta.
+            //var studentiNaIspitu = from stud in studenti
+            //                       from ispit in stud.Ispiti
+            //                       where ispit.Naziv == imePredmeta
+            //                       select stud;
 
             //foreach (var s in studentiNaIspitu)
-            //    Console.WriteLine(s.Ime);
+            //    Console.WriteLine(s.ImePrezime);
 
             Console.WriteLine($"Studenti koji su pali ispit iz predmeta {imePredmeta}:");
             // TODO:03d Napisati upit koji će rezultata prethodnog upita vratiti sve studente koji su pali ispit iz zadanog predmeta.
