b1307cc26a5914b34390b8589274392727ea07dd
Pavle Pavicic <ppavicic@dspace.hr>
2021-12-02 19:02:58 +0100

    Solved third task.

--- Zadatak2/Program.cs
+++ Zadatak2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,17 @@ namespace Vsite.CSharp.Labos4.Zadatak2
     {
         static void Main(string[] args)
         {
-
+            DataClasses1DataContext dc = new DataClasses1DataContext();
+            Table<student> studenti = dc.GetTable<student>();
+            var upit = from s in studenti
+                select new
+                {
+                    sID = s.stud_ID,
+                    Ime = s.ime + " " + s.prezime
+                };
+            Console.WriteLine("Popis svih studenata:");
+            foreach (var v in upit)
+                Console.WriteLine("ID:{0} Ime:{1}", v.sID, v.Ime);
         }
     }
 }
