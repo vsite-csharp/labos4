f21757d0a698d056538242b2fc4eee1b23f6ff73
Jagoda Jerkovic <jjerkovic@dspace.hr>
2021-12-02 18:39:32 +0100

    Zadatak1

--- Zadatak1/Program.cs
+++ Zadatak1/Program.cs
@@ -6,27 +6,62 @@ using System.Threading.Tasks;
 
 namespace Vsite.CSharp.Labos4.Zadatak1
 {
-    // TODO:03a Definirati klasu koja predstavlja studenta i klasu koja predstavlja ispit.
+    // 03a Definirati klasu koja predstavlja studenta i klasu koja predstavlja ispit.
     // Klasa Ispit sadrži članove Naziv i Ocjena, a klasa Student sadrži članove ImePrezime i listu Ispit[] ispita na koje je student izašao.
+    class Ispit
+    {
+        public string Naziv { get; set; }
+
+        public int Ocjena { get; set; }
+
+    }
+
+
+    class Student
+    {
+        public string ImePrezime { get; set; }
+
+        public Ispit[] Ispiti { get; set; }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            // TODO:03b Zadati listu studenata s popisom ispita na koje je izašao.
+            // 03b Zadati listu studenata s popisom ispita na koje je izašao.
+
+            var studenti = new List<Student>
+            {
+                new Student{ ImePrezime = "Pero Perić",
+                    Ispiti = new Ispit[]{ new Ispit{Naziv = "Matematika", Ocjena = 1} } },
+                new Student{ ImePrezime = "Pero Kvrgić",
+                    Ispiti = new Ispit[]{ new Ispit{Naziv = "Fizika", Ocjena = 5}, new Ispit{ Naziv = "Matematika", Ocjena=3 } } },
+                new Student{ ImePrezime = "Lana Jurčević",
+                    Ispiti = new Ispit[]{ new Ispit{Naziv = "Vjeronauk", Ocjena = 2}, new Ispit{ Naziv = "Matematika", Ocjena=3 } } },
+                new Student{ ImePrezime = "Mate Mišo Kovač",
+                    Ispiti = new Ispit[]{ new Ispit{Naziv = "Vjeronauk", Ocjena = 3}, new Ispit{ Naziv = "Povijest", Ocjena=4 } } },                
+                new Student{ ImePrezime = "Niko Kovač",
+                    Ispiti = new Ispit[]{  } },
+
+            };
 
             string imePredmeta = "Matematika";
             Console.WriteLine("Studenti koji su izašli na ispit iz predmeta {imePredmeta}");
-            // TODO:03c Napisati upit koji će vratiti popis svih studenata koji su izašli na ispit iz zadanog predmeta.
-
-            //foreach (var s in studentiNaIspitu)
-            //    Console.WriteLine(s.Ime);
+            // 03c Napisati upit koji će vratiti popis svih studenata koji su izašli na ispit iz zadanog predmeta.
+            var studentiNaIspitu = from student in studenti from ispit in student.Ispiti where ispit.Naziv == imePredmeta select student;
+            foreach (var s in studentiNaIspitu)
+                Console.WriteLine(s.ImePrezime);
 
             Console.WriteLine($"Studenti koji su pali ispit iz predmeta {imePredmeta}:");
-            // TODO:03d Napisati upit koji će rezultata prethodnog upita vratiti sve studente koji su pali ispit iz zadanog predmeta.
+            // 03d Napisati upit koji će rezultata prethodnog upita vratiti sve studente koji su pali ispit iz zadanog predmeta.
+            var studentiPaliIspit = from student in studentiNaIspitu
+                                    from ispit in student.Ispiti
+                                    where ispit.Ocjena == 1
+                                    select new { student, ispit };
 
-            //foreach (var s in studentiPaliIspit)
-            //    Console.WriteLine($"{s.student.Ime} je pao {s.ispit.Naziv}");
+            foreach (var s in studentiPaliIspit)
+            Console.WriteLine($"{s.student.ImePrezime} je pao {s.ispit.Naziv}");
+            Console.ReadKey();
         }
     }
 }
