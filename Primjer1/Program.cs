659172520f7ccd2c96929411434ad6665f11b7f3
Vicko Cagalj <vickocagalj@gmail.com>
2021-12-02 17:56:10 +0100

    Resolve Primjer1

--- Primjer1/Program.cs
+++ Primjer1/Program.cs
@@ -14,10 +14,12 @@ namespace Vsite.CSharp.Labos4.Primjer1
         {
             int[] brojevi = { 2, 7, 1, 12, 5, 9, 78, 4, 11, 13 };
             // TODO:01 Napisati upit koji će iz gornje liste dohvatiti samo brojeve veće od 10 
+            var rezUpita = from broj in brojevi 
+                                         where broj > 10
+                                         select broj;
 
-
-            //foreach (var s in rezUpita)
-            //    Console.Write(s.ToString() + " ");
+            foreach (var s in rezUpita)
+                Console.Write(s.ToString() + " ");
 
             Console.WriteLine();
             Console.ReadKey(false);
