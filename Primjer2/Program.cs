3b052f264cc5cb44a9d74c0fc5275eb8b648f47e
Andrea Vrljic <andrea31542@gmail.com>
2021-12-02 18:06:15 +0100

    Resolve 1 and 2

--- Primjer2/Program.cs
+++ Primjer2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -50,8 +51,17 @@ namespace Vsite.CSharp.Labos4.Primjer2
 
             // TODO:02 Napisati upit koji će iz popisa najboljih učenika prikazati samo one za koje u listi profesora naveden razrednik
 
-            //foreach (var s in upit)
-            //    Console.WriteLine("Učeniku {0} iz razreda {1} razrednik je {2}", s.ImePrezimeUčenika, s.RazredUčenika, s.ImePrezimeProfesora);
+            //var upit = from profesor in listaProfesora
+            //    from učenik in popisNajboljihUčenika
+            //    select new {učenik.ImePrezimeUčenika, učenik.RazredUčenika, profesor.ImePrezimeProfesora};
+
+            var upit = from učenik in popisNajboljihUčenika  
+                                                                from profesor in listaProfesora
+                                                                where profesor.JeRazrednikRazredu == učenik.RazredUčenika
+                                                                  select new { učenik.ImePrezimeUčenika, učenik.RazredUčenika, profesor.ImePrezimeProfesora };
+            
+            foreach (var s in upit)
+                Console.WriteLine("Učeniku {0} iz razreda {1} razrednik je {2}", s.ImePrezimeUčenika, s.RazredUčenika, s.ImePrezimeProfesora);
 
             Console.ReadKey(false);
         }
