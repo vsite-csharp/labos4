c8f6b7830a61b658d40dc78763e3fa6499fcab40
Antonio Brkic <ABrkic@dspace.hr>
2021-12-02 18:06:06 +0100

    Resolve primjer 2

--- Primjer2/Program.cs
+++ Primjer2/Program.cs
@@ -37,21 +37,29 @@ namespace Vsite.CSharp.Labos4.Primjer2
     {
         static void Main(string[] args)
         {
-            List<Profesor> listaProfesora = new List<Profesor> {
-                new Profesor{ ImePrezimeProfesora = "Dubravka Milić",JeRazrednikRazredu="3.b"},
-                new Profesor{ ImePrezimeProfesora = "Dragica Pribić", JeRazrednikRazredu="4.h"},
-                new Profesor{ ImePrezimeProfesora = "Miroslav Šašić", JeRazrednikRazredu="4.k"}
+            List<Profesor> listaProfesora = new List<Profesor>
+            {
+                new Profesor {ImePrezimeProfesora = "Dubravka Milić", JeRazrednikRazredu = "3.b"},
+                new Profesor {ImePrezimeProfesora = "Dragica Pribić", JeRazrednikRazredu = "4.h"},
+                new Profesor {ImePrezimeProfesora = "Miroslav Šašić", JeRazrednikRazredu = "4.k"}
             };
-            List<Učenik> popisNajboljihUčenika = new List<Učenik> {
-                new Učenik{ ImePrezimeUčenika="Ana Anić", RazredUčenika="1.a"},
-                new Učenik{ ImePrezimeUčenika="Ivo Matić", RazredUčenika="3.h"},
-                new Učenik{ ImePrezimeUčenika="Petra Petrić", RazredUčenika="4.h"}
+            List<Učenik> popisNajboljihUčenika = new List<Učenik>
+            {
+                new Učenik {ImePrezimeUčenika = "Ana Anić", RazredUčenika = "1.a"},
+                new Učenik {ImePrezimeUčenika = "Ivo Matić", RazredUčenika = "3.h"},
+                new Učenik {ImePrezimeUčenika = "Petra Petrić", RazredUčenika = "4.h"}
             };
 
-            // TODO:02 Napisati upit koji će iz popisa najboljih učenika prikazati samo one za koje u listi profesora naveden razrednik
+            // 02 Napisati upit koji će iz popisa najboljih učenika prikazati samo one za koje u listi profesora naveden razrednik
 
-            //foreach (var s in upit)
-            //    Console.WriteLine("Učeniku {0} iz razreda {1} razrednik je {2}", s.ImePrezimeUčenika, s.RazredUčenika, s.ImePrezimeProfesora);
+            //var upit = from profesor in listaProfesora from učenik in popisNajboljihUčenika select new { učenik.ImePrezimeUčenika, učenik.RazredUčenika, profesor.ImePrezimeProfesora};
+            var upit = from učenik in popisNajboljihUčenika 
+                from profesor in listaProfesora 
+                where profesor.JeRazrednikRazredu == učenik.RazredUčenika 
+                select new { učenik.ImePrezimeUčenika, učenik.RazredUčenika, profesor.ImePrezimeProfesora };
+
+            foreach (var s in upit)
+                Console.WriteLine("Učeniku {0} iz razreda {1} razrednik je {2}", s.ImePrezimeUčenika, s.RazredUčenika, s.ImePrezimeProfesora);
 
             Console.ReadKey(false);
         }
